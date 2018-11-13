91953806907b08daf75947ad0e04ded0ca99aae5
Yasunori Endo <jo7ueb@gmail.com>
2018-11-14 03:53:14 +0900

    show percentage

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -27,14 +27,19 @@ int main(int argc, char **argv)
     const double w = cap.get(CV_CAP_PROP_FRAME_WIDTH);
     const double h = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
     const double fps = cap.get(CV_CAP_PROP_FPS);
+    const double length = cap.get(CV_CAP_PROP_FRAME_COUNT);
     std::cout << file_name << "(" << w << "x" << h
               << " " << fps << "FPS) opened." << std::endl;
 
     // publish each video frame
     ros::Rate rate(fps);
+    size_t frameIdx = 0;
     while(ros::ok()) {
         cv::Mat frame;
         cap >> frame;
+
+        const double ratio = frameIdx / length;
+        std::cout << "frame#" << frameIdx++ << " (" << ratio*100 << "%)\r";
 
         if (frame.empty()) {
             std::cout << "reach to the end" << std::endl;
